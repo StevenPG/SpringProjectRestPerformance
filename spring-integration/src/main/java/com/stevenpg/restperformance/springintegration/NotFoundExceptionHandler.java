a96d61a1680a2a9d2f6ffc77f72d42c3ad2803ea
StevenPG <steven.p.gantz@gmail.com>
2020-08-29 13:08:23 -0400

    Add fully working spring integration example

--- spring-integration/src/main/java/com/stevenpg/restperformance/springintegration/NotFoundExceptionHandler.java
+++ spring-integration/src/main/java/com/stevenpg/restperformance/springintegration/NotFoundExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.stevenpg.restperformance.springintegration;
 
-import org.springframework.integration.transformer.Transformer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Component
-public class NotFoundExceptionHandler implements Transformer
+public class NotFoundExceptionHandler
 {
-
-    @Override
-    public Message<?> transform(Message<?> message) {
-        return null;
+    public Message<?> transform(ErrorMessage message) {
+        if (message.getPayload().getCause() instanceof HttpClientErrorException.NotFound) {
+            var entity = new ResponseEntity<>("Entity not found.", HttpStatus.NOT_FOUND);
+            return MessageBuilder.withPayload(entity).copyHeaders(message.getHeaders()).build();
+        }
+        else {
+            // This is taking many many minutes just to handle the exception properly...
+            return MessageBuilder.withPayload(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR))
+                    .copyHeaders(message.getHeaders()).build();
+        }
     }
 }
