bc882796b60624d680d509877dab11603c3cb604
Steven Gantz <steven.p.gantz@gmail.com>
2020-08-30 12:30:57 -0400

    add simple terraform template

---
+++ terraform/terraform.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_key_pair" "ubuntu" {
+  key_name   = "ubuntu"
+  public_key = file("key.pub")
+}
+
+resource "aws_security_group" "ubuntu" {
+  name        = "ubuntu-security-group"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  key_name      = aws_key_pair.ubuntu.key_name
+  ami           = "ami-03ba3948f6c37a4b0"
+  instance_type = "t2.micro"
+  name = "deployment-enviroment"
+
+  tags = {
+    Name = "deployment"
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.ubuntu.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("key")
+    host        = self.public_ip
+  }
+
+}
+
+resource "aws_instance" "ubuntu" {
+  key_name      = aws_key_pair.ubuntu.key_name
+  ami           = "ami-03ba3948f6c37a4b0"
+  instance_type = "t2.micro"
+  name = "performance-testing-enviroment"
+
+  tags = {
+    Name = "performance-testing"
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.ubuntu.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("key")
+    host        = self.public_ip
+  }
+
+}
